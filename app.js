12c5e74a2609cf63406cabfb1952f8992b4f7896
Jai-10 <jai.vaidya18@gmail.com>
2023-03-10 19:24:40 +0530

    updated start script

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const morgan = require('morgan')
+const session = require('express-session')
 
 const app = express()
 
-app.use(session({
+app.use( userSession({
     resave: true,
     saveUninitialized: true,
     secret: ['secret_key']
-}))
+}) )
 
 
 app.use(cors({origin: '*'}))
@@ -29,7 +30,6 @@ mongoose.connect("mongodb+srv://jai:jai@cluster0.3wvybhi.mongodb.net/?retryWrite
 const loginRoute = require('./api/routes/login')
 const signupRoute = require('./api/routes/signup')
 const logoutRoute = require('./api/routes/logout')
-const session = require('express-session')
 
 app.use('/login', loginRoute)
 app.use('/signup', signupRoute)
